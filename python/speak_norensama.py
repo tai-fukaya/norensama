1d0e3188cb7183f4e19fdd7f7aec1cf98882de7b
Tai Fukaya <taifuun.in@gmail.com>
2018-10-14 07:56:46 +0900

    Add twitterbot

--- python/speak_norensama.py
+++ python/speak_norensama.py
@@ -2,17 +2,26 @@
 import threading
 import time
 
+import _secretkey as config
 from speaker import Speaker
+from twitter import Twitter
 
 class Norensama(object):
 
     def __init__(self):
         self._speaker = Speaker()
+        self._twitter = Twitter({
+            "consumer_key": config.TWITTER_CONSUMER_KEY,
+            "consumer_secret": config.TWITTER_CONSUMER_SECRET,
+            "access_token": config.TWITTER_ACCESS_TOKEN,
+            "access_token_secret": config.TWITTER_ACCESS_TOKEN_SECRET
+        })
     
     def main(self):
         start = time.time()
         print("main")
         self._speaker.say("yureta")
+        self._twitter.tweet("ゆれた")
         print(time.time() - start)
         self._speaker.finish()
 
--- python/twitter.py
+++ python/twitter.py
@@ -1,2 +1,26 @@
 #-*-coding:utf-8-*-
 # https://qiita.com/bakira/items/00743d10ec42993f85eb
+# https://su-gi-rx.com/archives/942#3_StreamListener
+from requests_oauthlib import OAuth1Session
+
+TWEET_POST_URL = "https://api.twitter.com/1.1/statuses/update.json"
+
+class Twitter(object):
+
+    def __init__(self, config):
+        self.consumer_key = config.get("consumer_key")
+        self.consumer_secret = config.get("consumer_secret")
+        self.access_token = config.get("access_token")
+        self.access_token_secret = config.get("access_token_secret")
+
+        self._session = OAuth1Session(
+            self.consumer_key,self.consumer_secret,
+            self.access_token, self.access_token_secret
+        )
+
+    def tweet(self, message):
+        print("tweet:{}".format(message))
+
+        # params = {"status": message}
+        # res = self._session.post(TWEET_POST_URL, params = params)
+        # print(res)
