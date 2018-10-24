04b8ffa31a2cdc27912ddff8527af7893c115100
Tai Fukaya <taifuun.in@gmail.com>
2018-10-24 22:37:16 +0900

    [WIP] Tune up: motion sensor

--- nodejs/motion_sensor_sample.js
+++ nodejs/motion_sensor_sample.js
@@ -1,12 +1,6 @@
-/**
- * 人感センサーを取得する
- * 人が通ったら、一定期間、LEDを光らせる
- */
 require('date-utils');
-const express = require('express');
-const serveIndex = require('serve-index');
-const WebSocketClient = require('websocket').w3cwebsocket;
 const Obniz = require('obniz');
+const WebSocketClient = require('websocket').w3cwebsocket;
 
 const config = require('./_secret');
 
@@ -74,15 +68,6 @@ class MotionSensorObniz {
     }
 }
 
-// ローカルサーバーを立ち上げて、ここでウェブソケットで通信する
-const app = express();
-app.use(express.static(__dirname + '/server'));
-app.use(serveIndex(__dirname + '/server', {icons: true}));
-const PORT = 8000;
-app.listen(PORT);
-console.log(require('os').networkInterfaces().en0[1].address, PORT);
-// TODO Facebook messanger にIPを流す
-
 let client = new WebSocketClient('ws://localhost:6700');
 
 let mo0 = new MotionSensorObniz(
@@ -99,42 +84,25 @@ mo0.start(client, [{
     gnd: 6, signal: 7, vcc: 8
 }]);
 
-// accelerometer
-let acc0 = new Obniz("6364-1751");
-let accValueArray = [];
 
-acc0.onconnect = async function () {
-    console.log("connect", acc0.id);
-}
+/*
 
-acc0.repeat(async function() {
-    // accelerometer
-    let sensor = acc0.wired("KXR94_2050", {x: 2, y: 1, z: 0, gnd:10, vcc:11 });
-    let accValues = await sensor.getWait();
-    accValueArray.push(accValues);
-    if (accValueArray.length > 10) {
-        let begin = accValueArray.length - 10;
-        accValueArray = accValueArray.slice(begin);
-    }
-    let count = 0;
-    let averageAccValues = {x: 0, y: 0, z:0};
-    for (let acc of accValueArray) {
-        if (isNaN(acc.x + acc.y + acc.z)) {
-            continue;
-        }
-        count++;
-        averageAccValues.x += acc.x;
-        averageAccValues.y += acc.y;
-        averageAccValues.z += acc.z;
-    }
-    averageAccValues.x /= count;
-    averageAccValues.y /= count;
-    averageAccValues.z /= count;
+Sの直後に、Sじゃない場合、チェックする
+000 S
+100 O
+010 S
+001 I
+012 I
+021 O
+120 I
+102 I
+210 O
+201 O
+123 I
+132 I
+213 O
+231 O
+312 I
+321 O
 
-    if (client.readyState !== client.CLOSED) {
-        client.send(["acc", 0, averageAccValues.x, averageAccValues.y, averageAccValues.z].join(","));
-    }
-    // console.log((new Date()).toFormat('YYYYMMDDHH24MISS'));
-    console.log(averageAccValues.x.toFixed(3), averageAccValues.y.toFixed(3), averageAccValues.z.toFixed(3));
-    // oscClient.send("/acc", [0, averageAccValues.x, averageAccValues.y, averageAccValues.z].join(","))
-});
+*/
