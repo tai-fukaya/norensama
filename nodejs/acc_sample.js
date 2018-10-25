38dc4273bb744217b93267d8240df579a8677752
Tai Fukaya <taifuun.in@gmail.com>
2018-10-25 23:27:19 +0900

    Update acc sensor

--- nodejs/acc_sample.js
+++ nodejs/acc_sample.js
@@ -1,40 +1,37 @@
 const Obniz = require('obniz');
+const osc = require('node-osc');
+
+let oscClient = new osc.Client("127.0.0.1", 12000);
 
 let acc0 = new Obniz("6364-1751");
-let accValueArray = [];
 
-acc0.repeat(async function() {
-    // accelerometer
-    let sensor = acc0.wired("KXR94_2050", {x: 2, y: 1, z: 0, gnd:10, vcc:11 });
-    let accValues = await sensor.getWait();
-    accValueArray.push(accValues);
-    if (accValueArray.length > 10) {
-        let begin = accValueArray.length - 10;
-        accValueArray = accValueArray.slice(begin);
-    }
-    let count = 0;
-    let averageAccValues = {x: 0, y: 0, z:0};
-    for (let acc of accValueArray) {
-        if (isNaN(acc.x + acc.y + acc.z)) {
-            continue;
-        }
-        count++;
-        averageAccValues.x += acc.x;
-        averageAccValues.y += acc.y;
-        averageAccValues.z += acc.z;
-    }
-    averageAccValues.x /= count;
-    averageAccValues.y /= count;
-    averageAccValues.z /= count;
+acc0.onconnect = async () => {
+    console.log('connect');
+    let sensor = acc0.wired("KXR94-2050", {vcc:0, gnd:1, x:2, y:3, z:4 });
+    sensor.onChange = values => {
+        // 0.7 ~ -1.4
+        // let x = convert(values.x, -1.5, 0.75, -0.98, 0.98);
+        // let y = convert(values.y, -1.5, 0.75, -0.98, 0.98);
+        // let z = convert(values.z, -0.98, 0.6, -0.98, 0.98);
+        let x = values.x;
+        let y = values.y;
+        let z = values.z;
 
-    console.log(averageAccValues.x.toFixed(3), averageAccValues.y.toFixed(3), averageAccValues.z.toFixed(3));
-    
-});
+        console.log(`${x.toFixed(3)},${y.toFixed(3)},${z.toFixed(3)}`);
+        let message = new osc.Message("/acc");
+        message.append(x);
+        message.append(y);
+        message.append(z);
+        oscClient.send(message);
+    };
+}
 
-acc0.onconnect = async function() {
+acc0.onclose = async () => {
 
 }
 
-acc0.onclose = async function() {
-
+function convert(v, bmi, bmx, ami, amx) {
+    v = Math.min(bmx, v);
+    v = Math.max(bmi, v);
+    return ami + (v - bmi) * (amx - ami)/(bmx - bmi);
 }
