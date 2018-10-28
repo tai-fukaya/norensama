34367d915e12e1e5f1293e870c509547f1ce2835
Owner <owner@MA1030.local>
2018-10-28 18:13:04 +0900

    Add twitter answer

--- python/action/coredo_answer_gehin.py
+++ python/action/coredo_answer_gehin.py
@@ -4,21 +4,19 @@ import time
 
 from action_base import ActionBase
 
-class CoredoAnswerIkku(ActionBase):
+class CoredoAnswerGehin(ActionBase):
 
     REST_DURATION = 30.
     SERIFS = [
-        "閑さや、コレドにそんな、ものはない",
-        "秋雨を、あつめて早し、日本橋川（字余り）",
-        "秋深き、隣はいつもの、暖簾じゃぞ",
+        "日本橋を歩く者は上品でないとな",
     ]
 
     def __init__(self, speaker):
-        super(CoredoAnswerIkku, self).__init__(speaker)
+        super(CoredoAnswerGehin, self).__init__(speaker)
 
     def check(self, data):
         return random.random() > 0
-        #ツイッターで一句読んでと言われたら
+        #ツイッターで下品な事を言われたら
 
     def run(self, data):
         serif = self.SERIFS[int(random.random()*len(self.SERIFS))]
--- python/main.py
+++ python/main.py
@@ -10,7 +10,7 @@ from status import StatusManager
 from speaker import Speaker
 from ifttt import Ifttt
 # Action
-from action import Yureyure, Hello, Joke, TimeSignal, ForceSpeak, CoredoIntroduction, CoredoIntroductionAM9, CoredoIntroductionLunch, CoredoIntroductionNight, CoredoBoyaki, CoredoAnswerHanashi, CoredoAnswerAisatsu, CoredoAnswerSonota, CoredoIntroductionNight, CoredoBoyaki, CoredoAnswerHanashi, CoredoAnswerAisatsu, CoredoAnswerDare, CoredoAnswerIkku
+from action import Yureyure, Hello, Joke, TimeSignal, ForceSpeak, CoredoIntroduction, CoredoIntroductionAM9, CoredoIntroductionLunch, CoredoIntroductionNight, CoredoBoyaki, CoredoAnswerHanashi, CoredoAnswerAisatsu, CoredoAnswerSonota, CoredoIntroductionNight, CoredoBoyaki, CoredoAnswerHanashi, CoredoAnswerAisatsu, CoredoAnswerDare, CoredoAnswerIkku, CoredoAnswerGehin, CoredoAnswerSize, CoredoAnswerToshi
 from action.blow import Yurayura, Soyosoyo, Byubyu
 
 
@@ -38,6 +38,9 @@ class Norensama(object):
             CoredoAnswerSonota(self._speaker),
             CoredoAnswerDare(self._speaker),
             CoredoAnswerIkku(self._speaker),
+            CoredoAnswerGehin(self._speaker),
+            CoredoAnswerSize(self._speaker),
+            CoredoAnswerToshi(self._speaker),
         ]
         self._blow_actions = [
             # よわい
@@ -68,7 +71,7 @@ class Norensama(object):
         self.select_blow_action()
         selected_time = time.time()
 
-        self._actions[13].run({})
+        self._actions[16].run({})
         
         while True:
             time.sleep(1.)
