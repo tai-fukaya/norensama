725a5b48ab070f2418856411b0414bb00b812e68
Tai Fukaya <taifuun.in@gmail.com>
2018-10-28 19:46:22 +0900

    風のでるタイミングをすくなめに

--- python/main.py
+++ python/main.py
@@ -10,7 +10,7 @@ from status import StatusManager
 from speaker import Speaker
 from ifttt import Ifttt
 # Action
-from action import Yureyure, Hello, Joke, TimeSignal, ForceSpeak, CoredoIntroduction, CoredoIntroductionAM9, CoredoIntroductionLunch, CoredoIntroductionNight, CoredoBoyaki, CoredoAnswerHanashi, CoredoAnswerAisatsu, CoredoAnswerSonota, CoredoIntroductionNight, CoredoBoyaki, CoredoAnswerHanashi, CoredoAnswerAisatsu, CoredoAnswerDare, CoredoAnswerIkku, CoredoAnswerGehin, CoredoAnswerSize, CoredoAnswerToshi
+from action import Hello, Joke, TimeSignal, ForceSpeak, CoredoIntroduction, CoredoIntroductionAM9, CoredoIntroductionLunch, CoredoIntroductionNight, CoredoBoyaki, CoredoAnswerHanashi, CoredoAnswerAisatsu, CoredoAnswerSonota, CoredoIntroductionNight, CoredoBoyaki, CoredoAnswerHanashi, CoredoAnswerAisatsu, CoredoAnswerDare, CoredoAnswerIkku, CoredoAnswerGehin, CoredoAnswerSize, CoredoAnswerToshi
 from action.blow import Yurayura, Soyosoyo, Byubyu
 
 
@@ -24,15 +24,22 @@ class Norensama(object):
 
         self._status = StatusManager()
         self._actions = [
-            Yureyure(self._speaker),
+            # 風の音系
+            Soyosoyo(self._speaker),
+            Yurayura(self._speaker),
+            Byubyu(self._speaker),
+            # あいさつ
             Hello(self._speaker),
             Joke(self._speaker),
             TimeSignal(self._speaker),
+            # コレド紹介
             CoredoIntroduction(self._speaker),
             CoredoIntroductionAM9(self._speaker),
             CoredoIntroductionLunch(self._speaker),
             CoredoIntroductionNight(self._speaker),
+            # クソウンチク
             CoredoBoyaki(self._speaker),
+            # ツイッター
             CoredoAnswerHanashi(self._speaker),
             CoredoAnswerAisatsu(self._speaker),
             CoredoAnswerSonota(self._speaker),
@@ -42,24 +49,11 @@ class Norensama(object):
             CoredoAnswerSize(self._speaker),
             CoredoAnswerToshi(self._speaker),
         ]
-        self._blow_actions = [
-            # よわい
-            Soyosoyo(self._speaker),
-            # 横揺れ
-            Yurayura(self._speaker),
-            # 波
-            Byubyu(self._speaker)
-        ]
+
         # セリフの強制実行
         self._force_speak_action = ForceSpeak(self._speaker)
 
         self._blow_action_index = 1
-
-    def select_blow_action(self, data = {}):
-        acc = data.get("accelerometer")
-        if acc is None:
-            return
-        self._blow_action_index = acc.get("status", 0)
 
     def main(self):
         print("main")
@@ -68,8 +62,6 @@ class Norensama(object):
         status_thread.start()
 
         self._status.set_serif_names(self._force_speak_action.get_serif_names())
-        self.select_blow_action()
-        selected_time = time.time()
 
         self._actions[16].run({})
         
@@ -77,19 +69,11 @@ class Norensama(object):
             time.sleep(1.)
 
             data = self._status.get_data()
-            # 10分に一回、ゆれの音を決める
-            if time.time() - selected_time > 60.:
-                self.select_blow_action(data)
-                # その音で、IFTTT
-
-            # 風のまね
-            self._blow_actions[self._blow_action_index].run(data)
 
             # 強制起動もここでやる
             if self._force_speak_action.check(data):
                 message = self._force_speak_action.run(data)
                 # 一定時間発言していないキーワードだったら、ツイート
-
 
             # RT、フォロー、された場合は、ここでいう
             if random.random() > .95:
