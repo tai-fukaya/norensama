4ce89fd13bcc14b8bf3e735a59fc571c92fa1970
daifuku <fis10sai@i.softbank.jp>
2018-10-28 19:13:56 +0900

    Update weather.py method argument
    Weatherクラスの時間ごとに取得するメソッドと、日付ごとに取得するメソッドに引数を追加しました。

--- python/sample_weather.py
+++ python/sample_weather.py
@@ -7,8 +7,10 @@ weather = Weather({
     "api_key": config.WEATHER_APIKEY
 })
 
+hour = 2
+day = 1
 print '現在の天気:', weather.get_current_weather()
 print '現在の温度:', weather.get_current_temperature()
-print '2時間後の天気:', weather.get_hourly_weather()
-print '2時間後の温度:', weather.get_hourly_temperature()
-print '明日の天気:', weather.get_tommorow_weather()
+print str(hour), '時間後の天気:', weather.get_hourly_weather(hour)
+print str(hour),'時間後の温度:', weather.get_hourly_temperature(hour)
+print str(day),'日後の天気:', weather.get_tommorow_weather(day)
