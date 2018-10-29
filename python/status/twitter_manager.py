bfe5b9213ae801951d0d560f5cceec768be640e5
daifuku <fis10sai@i.softbank.jp>
2018-10-29 21:30:46 +0900

    Update update method
    updateメソッドのfollowerの取得、リツイート数の取得、ハッシュタグツイートの取得、メンションツイートの取得についてのプログラムを追加また取得タイミングの調整。
    リツイート部分に関しては、ひとまずコメントアウトしています。

--- python/status/twitter_manager.py
+++ python/status/twitter_manager.py
@@ -12,10 +12,11 @@ class TwitterManager(object):
         self._mention_messages = []
 
         self._tweet_messages = []
-    
+
     def tweet(self, message):
         # FIXME 直近１時間で、すでに投稿したものかどうかチェックし、その場合、投稿はしない
         # １時間の制限になりそうな場合も、投稿しない
+        # tweet 100times/1min
         self._tweet_messages.append(message)
 
     def update(self):
@@ -35,20 +36,21 @@ class TwitterManager(object):
             if time.time() - last_retweet_time > 20.:
                 # self._has_retweet = self._has_retweet or self._twitter.has_retweet()
                 last_retweet_time = time.time()
-            # Follower 15times/15min
-            if time.time() - last_follower_time > 60.:
-                # self._has_follower = self._has_follower or self._twitter.has_follower()
+            # Follower 900times/15min
+            if time.time() - last_follower_time > 2.:
+                self._has_follower = self._has_follower or self._twitter.has_follower()
                 last_follower_time = time.time()
             # hash 180times/15min
             if time.time() - last_hashtag_time > 10.:
-                # self._hashtag_messages.extend(self._twitter.get_hashtags())
+                self._hashtag_messages.extend(self._twitter.get_hashtags("のれんさま"))
+                print(self._hashtag_messages)
                 last_hashtag_time = time.time()
-            # mention ?
-            if time.time() - last_mention_time > 5.:
-                # self._mention_messages.extend(self._twitter.get_mention_timeline())
+            # mention 75times/15min
+            if time.time() - last_mention_time > 6.:
+                self._mention_messages.extend(self._twitter.get_mention_timeline())
                 last_mention_time = time.time()
             time.sleep(1.)
-    
+
     def get_data(self):
         has_retweet = self._has_retweet
         has_follower = self._has_follower
@@ -65,5 +67,3 @@ class TwitterManager(object):
             "hashtag": len(hashtag_messages) > 0,
             "mentions": mention_messages
         }
-
-    
